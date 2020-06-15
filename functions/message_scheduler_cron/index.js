aea2b358aeb4c3725e5f2675d335f4fa0a44687d
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-06-15 21:16:50 +0530

    Fixes in Update Oauth

--- functions/message_scheduler_cron/index.js
+++ functions/message_scheduler_cron/index.js
@@ -47,9 +47,13 @@ const getAccessToken = async (app, userId) => {
     if (!response.length) {
       throw new Error("Cannot find details about that user.");
     }
-    const { zuid, accessToken, refreshToken, accessTokenExpires } = response[0][
-      config.oauthTableName
-    ];
+    const {
+      accessToken,
+      refreshToken,
+      accessTokenExpires,
+      ROWID,
+    } = response[0][config.oauthTableName];
+    console.log(refreshToken);
     if (checkExpire(parseInt(accessTokenExpires), Date.now())) {
       return accessToken;
     } else {
@@ -67,8 +71,8 @@ const getAccessToken = async (app, userId) => {
           },
         }
       );
-      const { access_token, expires_in } = tokenResponse;
-      updateAccessToken(app, zuid, access_token, expires_in);
+      const { access_token, expires_in } = tokenResponse.data;
+      await updateAccessToken(app, ROWID, access_token, expires_in);
       return access_token;
     }
   } catch (error) {
@@ -81,13 +85,18 @@ const checkExpire = (expiretime, currentTime) => {
   return Math.round(expiretime / 1000) > Math.round(currentTime / 1000);
 };
 
-const updateAccessToken = async (app, userId, accessToken, expires_in) => {
+const updateAccessToken = async (app, ROWID, accessToken, expires_in) => {
   try {
-    const zcql = app.zcql();
-    const newAccessTokenExpires = Date.now() + parseInt(expires_in) * 1000;
-    const query = `UPDATE ${config.oauthTableName} SET accessToken = ${accessToken}, accessTokenExpires = ${newAccessTokenExpires} WHERE zuid=${userId}`;
-    const response = await zcql.executeZCQLQuery(query);
-    console.log(response);
+    const datastore = app.datastore();
+    const table = datastore.table(config.oauthTableName);
+    const accessTokenExpires = Date.now() + parseInt(expires_in) * 1000;
+    const updatedRowData = {
+      accessToken,
+      accessTokenExpires,
+      ROWID,
+    };
+    const updateDbResp = await table.updateRow(updatedRowData);
+    console.log(updateDbResp);
   } catch (error) {
     console.log(error);
   }
