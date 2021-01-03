4282441ba14b5f67d2812ec6c2051b86be5a1e7e
Godwin <godwin.a@zohocorp.com>
2021-01-03 22:06:03 +0530

    automation account changes

--- functions/message_scheduler_cron/config/index.js
+++ functions/message_scheduler_cron/config/index.js
@@ -1,16 +1,13 @@
-const isDev = false;
-const baseUrl = isDev
-  ? "http://localhost:3000"
-  : "https://messagescheduler-716534823.development.zohocatalyst.com";
+const isDev = true;
+const baseUrl = `https://messagescheduler-701800584.${isDev ? "development" : "production"}.zohocatalyst.com`;
 const config = {
-  baseUrl,
-  client_id: "1000.U51MVSGKUIV3N5J06RRSQGJYUF7G6H",
-  client_secret: "00e05af97bba079f7b9bef9f3181bc000339ecf44e",
-  redirect_uri: `${baseUrl}/server/message_scheduler_router/oauth/callback`,
-  oauthTableName: "oauthManagement",
-  scheduledMessageTableName: "scheduledMessage",
-  encryptKey:
-    "8324d46a5e30b55abc8c525b79fa7cffd9f95490bb8a3798d4969202b712a051",
-  encryptAlgorithm: "aes-256-cbc",
+    baseUrl,
+    client_id: "1000.35X3R7HJSD7AIQZX4PP2CNH5LR5QGY",
+    client_secret: "4ecd44c1cec5e630d03298ecf07d482ab761efdbab",
+    redirect_uri: `${baseUrl}/server/message_scheduler_router/oauth/callback`,
+    oauthTableName: "oauthManagement",
+    scheduledMessageTableName: "scheduledMessage",
+    encryptKey: "8324d46a5e30b55abc8c525b79fa7cffd9f95490bb8a3798d4969202b712a051",
+    encryptAlgorithm: "aes-256-cbc",
 };
 module.exports = config;
--- functions/message_scheduler_router/config/index.js
+++ functions/message_scheduler_router/config/index.js
@@ -1,19 +1,15 @@
-const isDev = false;
-const baseUrl = isDev
-  ? "http://localhost:3000"
-  : "https://messagescheduler-716534823.development.zohocatalyst.com";
+const isDev = true;
+const baseUrl = `https://messagescheduler-701800584.${isDev ?"development":"production"}.zohocatalyst.com`;
 const config = {
-  baseUrl,
-  client_id: "1000.U51MVSGKUIV3N5J06RRSQGJYUF7G6H",
-  client_secret: "00e05af97bba079f7b9bef9f3181bc000339ecf44e",
-  redirect_uri: `${baseUrl}/server/message_scheduler_router/oauth/callback`,
-  oauthTableName: "oauthManagement",
-  scheduledMessageTableName: "scheduledMessage",
-  cronFunctionId: "6594000000020003",
-  encryptKey:
-    "8324d46a5e30b55abc8c525b79fa7cffd9f95490bb8a3798d4969202b712a051",
-  encryptAlgorithm: "aes-256-cbc",
-  extensionPublicKey:
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnoslSNRJ03qXa7AFW+G3qXAfA9vF4OaJcnETCLBAeGy54CSKhhv7zLMcNK3o7Gs71hRXFTvUcC2oCPTx+xZ0sZPKy5j91PwnJWKRMzmtqgXavD67O8IXKRnlX0WFYCw1UQRvcZlQzbWJAFO8I1LfHoNr0LvU9Sf3vevPvIcmSB9GEFxGXIxcl1tw+/vTBfkczk8BmLbyNXBkNtx1esK+wF1PVASj61thnC0vWtnx2AunsoH9GBGWqv76D90RW8yTOFmcL9r8reyliv5W01B3qPegLkKrJb7TnHNU+bz4E3FVZdBLH9dZE4oI4BDHThHwHAX1bNKmtXmXOXkZy0xVXQIDAQAB",
+    baseUrl,
+    client_id: "1000.35X3R7HJSD7AIQZX4PP2CNH5LR5QGY",
+    client_secret: "4ecd44c1cec5e630d03298ecf07d482ab761efdbab",
+    redirect_uri: `${baseUrl}/server/message_scheduler_router/oauth/callback`,
+    oauthTableName: "oauthManagement",
+    scheduledMessageTableName: "scheduledMessage",
+    cronFunctionId: "6740000000509004",
+    encryptKey: "8324d46a5e30b55abc8c525b79fa7cffd9f95490bb8a3798d4969202b712a051",
+    encryptAlgorithm: "aes-256-cbc",
+    extensionPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlWyl4ddTGKLTja0tLSzRt4O3J+Pd8mR5ao/8pnyB0QyefBBca25UzwjIinkq74voCYo6jKPhCtrXan2iqr4WWdmCTyrOHVvQKtcmhVJq9Zqy6fPd+gSOtXFA8qgJr7J1Xfm8GRTOkcGJs/vV4sFAOzPXHP96BwQKRnjny/IJcyppGCy3+N6iwm3/8mpPantXqqwJ9pKHC/pkEJX0hOUyS/SIvrOzxHmgparisjuTIgJlDeq5DUm0To2nSSqqoCvJX2RM4JwEwg/PlPvIvv9eE2ZLMewSbQMflbjErtZBLriYvJN9N45QDP1FfREo8G3M0ozm8nZwjEXVxj5TAh+cGwIDAQAB",
 };
 module.exports = config;
